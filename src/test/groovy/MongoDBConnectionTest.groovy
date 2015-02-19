881c578d06e55e8189dde00f5710a410827c0d3a
drobertus <david_robertus@yahoo.com>
2015-02-18 18:58:54 -0700

    test for connection
    Work with the connection, basic persistence

--- src/test/groovy/MongoDBConnectionTest.groovy
+++ src/test/groovy/MongoDBConnectionTest.groovy
@@ -1,25 +1,19 @@
 import com.mongodb.BasicDBObject;
-import com.mongodb.BulkWriteOperation;
-import com.mongodb.BulkWriteResult;
-import com.mongodb.Cursor;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
-import com.mongodb.ParallelScanOptions
-import com.mongodb.ServerAddress
-import spock.lang.Specification;
+import groovy.util.logging.Log
+import spock.lang.Specification
 
-import java.util.List;
-import java.util.Set;
+import java.text.SimpleDateFormat;
 
 import static org.junit.Assert.assertEquals
-
-import static java.util.concurrent.TimeUnit.SECONDS
 import static org.junit.Assert.assertFalse
 import static org.junit.Assert.assertTrue;
 
+@Log
 class MongoDBConnectionTest extends Specification {
 
 
@@ -27,33 +21,31 @@ class MongoDBConnectionTest extends Specification {
         expect:
 // To directly connect to a single MongoDB server (note that this will not auto-discover the primary even
 // if it's a member of a replica set:
-        // MongoClient mongoClient = new MongoClient();
-// or
-        // MongoClient mongoClient = new MongoClient("localhost");
+
 // or
         MongoClient mongoClient = new MongoClient("localhost", 27017);
 // or, to connect to a replica set, with auto-discovery of the primary, supply a seed list of members
         //MongoClient mongoClient = new MongoClient(Arrays.asList(new ServerAddress("localhost", 27017),
         //   new ServerAddress("localhost", 27018),
         //   new ServerAddress("localhost", 27019)));
-        def dbName = 'mydb'
-        DB db = mongoClient.getDB(dbName);
-
-        String name = db.getName()
-        assertEquals dbName, name
-
-        Set<String> knowColls = db.collectionNames
-        knowColls.each {
-            println it
-        }
-
-        DBCollection coll = db.getCollection('testData')
-        println("count is ${coll.count()} in testData");
-        DBCursor cur1 = coll.find()
-        while(cur1.hasNext()) {
-            println("val= ${cur1.next()}")
-        }
-        cur1.close()
+//        def dbName = 'mydb'
+//        DB db = mongoClient.getDB(dbName);
+//
+//        String name = db.getName()
+//        assertEquals dbName, name
+//
+//        Set<String> knowColls = db.collectionNames
+//        knowColls.each {
+//            println it
+//        }
+//
+//        DBCollection coll = db.getCollection('testData')
+//        println("count is ${coll.count()} in testData");
+//        DBCursor cur1 = coll.find()
+//        while(cur1.hasNext()) {
+//            println("val= ${cur1.next()}")
+//        }
+//        cur1.close()
 
 
         //TODO: test inserts, updated, deletes
@@ -88,34 +80,46 @@ class MongoDBConnectionTest extends Specification {
         //now lets add, update and delete a document in the collection
         assertEquals 0, testColl.count()
 
+        def sdf = new SimpleDateFormat('yyyy-MM-dd')
         def mapOfVals = [:]
-        mapOfVals <<  ['_id': 25]
+        mapOfVals <<  ['id': 25]
         mapOfVals << ['lastName':'Sanderson']
         mapOfVals << ['firstName': 'Thomas']
-        mapOfVals << ['dob': '2005-07-12']
+        mapOfVals << ['dob': sdf.parse('2005-07-12')]
 
         testColl.insert(new BasicDBObject(mapOfVals))
 
-        def obj2 = new BasicDBObject('_id', 34).append('lastName', 'Thorn')
-            .append('firstName', 'Marilyn').append('dob', '1968-03-22')
+        def bDate2 = sdf.parse('1968-03-22')
+        def obj2 = new BasicDBObject('id', 34).append('lastName', 'Thorn')
+            .append('firstName', 'Marilyn').append('dob', bDate2)
+            .append('birthNumber', 5.89d)
         testColl.insert(obj2)
+
+        def testDate = sdf.parse('1999-12-31')
 
         assertEquals 2, testColl.count()
         DBCursor cursor = testColl.find()
         def countReturned = 0
         while(cursor.hasNext()) {
             countReturned ++
-            println countReturned + ': ' + cursor.next().toString()
+            BasicDBObject theDoc = cursor.next()
+            println countReturned + ': ' + theDoc.toString()
+            Date dob = theDoc.get('dob')
+            if (dob.after(testDate)) {
+              println "${dob} is after ${testDate}"
+            }
+            else {
+                println "${dob} is before ${testDate}"
+            }
         }
         cursor.close()
-        def finder1 = testColl.find(new BasicDBObject('_id', 34))
+        def finder1 = testColl.find(new BasicDBObject('id', 34))
         def countReturned2 = 0
         BasicDBObject foundDoc
         while(finder1.hasNext()) {
             countReturned2 ++
             foundDoc = finder1.next()
             println countReturned2 + ': ' + foundDoc.toString()
-
         }
         finder1.close()
         assertEquals (1, countReturned2)
